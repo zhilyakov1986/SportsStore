bef6972ae4eb6e62d093190301ebfe40d1e333a0
Andrey Zhilyakov <zhilyakov1986@gmail.com>
2018-12-19 07:04:45 -0500

    libman added bootstrap

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -16,10 +16,11 @@ namespace SportsStore.Controllers
             repository = repo;
         }
 
-        public ViewResult List(int productPage = 1)
+        public ViewResult List(string category, int productPage = 1)
             => View(new ProductsListViewModel
             {
                 Products = repository.Products
+                    .Where(p => category == null || p.Category == category)
                     .OrderBy(p => p.ProductId)
                     .Skip((productPage - 1) * PageSize)
                     .Take(PageSize),
@@ -28,7 +29,8 @@ namespace SportsStore.Controllers
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
                     TotalItems = repository.Products.Count()
-                }
+                },
+                CurrentCategory = category
             });
     }
 }
